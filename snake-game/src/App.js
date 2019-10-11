96f31cdd42982f4a23fb9373ac8bdde2ed49ed76
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-10-11 17:28:07 +0530

    Added apple, snakehead and snake body to the grid

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -1,16 +1,44 @@
 import React, {useReducer} from 'react';
+
 import initState from './initState';
+import {Keys, move } from './Keys';
 
 const App=()=> {
-    const [state, dispatch] = useReducer(null,initState()); // todo: write the reducer
-    console.log("state: ", state);
+  const [state, dispatch] = useReducer(null,initState()); // todo: write the reducer
+  console.log("state: ", state);
+  // Set the style for the cells in the grid.
+  // More info on top of the program page.
+  const cellStyle = (cell) => {
+    const { snake, food, showGrid } = state;
+    let style = `cell `;
+    if (snake.head.row == cell.row && snake.head.col == cell.col) {
+      style= `cell head head-up`;
+      if (move == Keys.Left) {
+        style= `cell head head-left`;
+      } else if (move == Keys.Up) {
+        style= `cell head head-up`;
+      } else if (move == Keys.Right) {
+        style= `cell head head-right`;
+      } else if (move == Keys.Down) {
+        style= `cell head head-down`;
+      }
+    } else if (food.row == cell.row && food.col == cell.col)  {
+      style= `cell food`;
+    } else if (snake.tail.find(t => t.row === cell.row && t.col === cell.col)) {
+      style = `cell tail`;
+    }
+    
+    style = showGrid ? style + ' cell-border' : style;
+    return style;
+  }
     // Generate grid based on row X col
     const drawGrid = () => {
       const {grid} = state;
       return (
        grid.map((row, i) => {
          return row.map(cell => {
-           return <div key={cell.row+cell.col} className="cell cell-border"  />
+          let actorStyle = cellStyle(cell);
+          return <div key={cell.row+cell.col} className={actorStyle}  />
          });
        })
       );
---
+++ snake-game/src/Keys.js
@@ -0,0 +1,15 @@
+const Keys = {
+    Space: 32,
+    Left: 37,
+    Up: 38,
+    Right: 39,
+    Down: 40,
+    a: 65,  // left
+    w: 87,  // up
+    s: 83,  // down
+    d: 68   // right
+  }
+
+  var move = Keys.Right;
+
+  export {Keys, move };
